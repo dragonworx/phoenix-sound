b230d3d40a27b0d6cefa5f21ae11e423f858571f
dragonworx <dragonworxau@yahoo.com.au>
2025-09-17 18:08:39 +1000

    update layout

--- app/(default)/components/server/Footer.tsx
+++ app/(default)/components/server/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-600 text-white w-full p-2.5">
+    <footer className="bg-black text-white w-full p-2.5">
       <div className="text-center">
-        <p className="text-sm">© 2025 My Website. All rights reserved.</p>
+        <p className="text-sm">© 2025 Phoenix Sound. All rights reserved.</p>
       </div>
     </footer>
   );
--- app/(default)/components/server/Header.tsx
+++ app/(default)/components/server/Header.tsx
@@ -1,10 +1,12 @@
 export default function Header() {
   return (
-    <header className="bg-gray-800 text-white w-full p-2.5 sticky top-0 z-10">
+    <header className="bg-black text-white w-full p-2.5 sticky top-0 z-10">
       <div className="flex">
         <div className="flex items-center space-x-3">
-          <div className="w-8 h-8 bg-gray-600 rounded-full flex"></div>
-          <h1 className="text-xl font-bold">Welcome to My Website</h1>
+          <div className="w-8 h-8 bg-gray-600 rounded-full flex">
+            <img src="/img/logo.png" alt="Logo" className="w-8 h-8 rounded-full" />
+          </div>
+          <h1 className="text-xl font-bold">Phoenix Sound</h1>
         </div>
       </div>
     </header>
